a58144b1f1c18bb095d4dd5381370d4ed1b25b72
DiogoPinto13 <pinto.diogo2002@gmail.com>
2026-03-20 09:28:59 +0000

    removed some transformations etc to make it possible to compare to evodenss

--- adv_supervised_learning.py
+++ adv_supervised_learning.py
@@ -1,3 +1,239 @@
+# # Copyright 2023 solo-learn development team.
+
+# # Permission is hereby granted, free of charge, to any person obtaining a copy of
+# # this software and associated documentation files (the "Software"), to deal in
+# # the Software without restriction, including without limitation the rights to use,
+# # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# # Software, and to permit persons to whom the Software is furnished to do so,
+# # subject to the following conditions:
+
+# # The above copyright notice and this permission notice shall be included in all copies
+# # or substantial portions of the Software.
+
+# # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# # PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# # DEALINGS IN THE SOFTWARE.
+
+# import inspect
+# import logging
+# import os
+
+# import hydra
+# import torch
+# import torch.nn as nn
+# from omegaconf import DictConfig, OmegaConf
+# from pytorch_lightning import Trainer
+# from pytorch_lightning.callbacks import LearningRateMonitor
+# from pytorch_lightning.loggers import WandbLogger
+# from pytorch_lightning.loggers import TensorBoardLogger
+# from pytorch_lightning.strategies.ddp import DDPStrategy
+# from timm.data.mixup import Mixup
+# from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+
+# from solo.args.linear import parse_cfg
+# from solo.data.classification_dataloader import prepare_data
+# from solo.methods.base import BaseMethod
+# # from solo.methods.linear import LinearModel
+# from adv_linear  import LinearModel
+# from solo.utils.auto_resumer import AutoResumer
+# from solo.utils.checkpointer import Checkpointer
+# from solo.utils.misc import make_contiguous
+
+# try:
+#     from solo.data.dali_dataloader import ClassificationDALIDataModule
+# except ImportError:
+#     _dali_avaliable = False
+# else:
+#     _dali_avaliable = True
+
+
+# @hydra.main(version_base="1.2")
+# def main(cfg: DictConfig):
+#     OmegaConf.set_struct(cfg, False)
+#     cfg = parse_cfg(cfg)
+
+#     backbone_model = BaseMethod._BACKBONES[cfg.backbone.name]
+
+#     # initialize backbone
+#     backbone = backbone_model(method=cfg.pretrain_method, **cfg.backbone.kwargs)
+#     if cfg.backbone.name.startswith("resnet"):
+#         # remove fc layer
+#         backbone.fc = nn.Identity()
+#         cifar = cfg.data.dataset in ["cifar10", "cifar100"]
+#         if cifar:
+#             backbone.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
+#             backbone.maxpool = nn.Identity()
+    
+#     # check if mixup or cutmix is enabled
+#     mixup_func = None
+#     mixup_active = cfg.mixup > 0 or cfg.cutmix > 0
+#     if mixup_active:
+#         logging.info("Mixup activated")
+#         mixup_func = Mixup(
+#             mixup_alpha=cfg.mixup,
+#             cutmix_alpha=cfg.cutmix,
+#             cutmix_minmax=None,
+#             prob=1.0,
+#             switch_prob=0.5,
+#             mode="batch",
+#             label_smoothing=cfg.label_smoothing,
+#             num_classes=cfg.data.num_classes,
+#         )
+#         # smoothing is handled with mixup label transform
+#         loss_func = SoftTargetCrossEntropy()
+#     elif cfg.label_smoothing > 0:
+#         loss_func = LabelSmoothingCrossEntropy(smoothing=cfg.label_smoothing)
+#     else:
+#         loss_func = torch.nn.CrossEntropyLoss()
+
+#     model = LinearModel(backbone, loss_func=loss_func, mixup_func=mixup_func, cfg=cfg)
+#     make_contiguous(model)
+#     # can provide up to ~20% speed up
+#     if not cfg.performance.disable_channel_last:
+#         model = model.to(memory_format=torch.channels_last)
+
+    
+#     if cfg.data.format == "dali":
+#         val_data_format = "image_folder"
+#     else:
+#         val_data_format = cfg.data.format
+
+#     train_loader, val_loader = prepare_data(
+#         cfg.data.dataset,
+#         train_data_path=cfg.data.train_path,
+#         val_data_path=cfg.data.val_path,
+#         data_format=val_data_format,
+#         batch_size=cfg.optimizer.batch_size,
+#         num_workers=cfg.data.num_workers,
+#         auto_augment=cfg.auto_augment,
+#         data_fraction=cfg.data.fraction
+#     )
+
+#     if cfg.data.format == "dali":
+#         assert (
+#             _dali_avaliable
+#         ), "Dali is not currently avaiable, please install it first with pip3 install .[dali]."
+
+#         assert not cfg.auto_augment, "Auto augmentation is not supported with Dali."
+
+#         dali_datamodule = ClassificationDALIDataModule(
+#             dataset=cfg.data.dataset,
+#             train_data_path=cfg.data.train_path,
+#             val_data_path=cfg.data.val_path,
+#             num_workers=cfg.data.num_workers,
+#             batch_size=cfg.optimizer.batch_size,
+#             data_fraction=cfg.data.fraction,
+#             dali_device=cfg.dali.device,
+#         )
+
+#         # use normal torchvision dataloader for validation to save memory
+#         dali_datamodule.val_dataloader = lambda: val_loader
+
+#     # 1.7 will deprecate resume_from_checkpoint, but for the moment
+#     # the argument is the same, but we need to pass it as ckpt_path to trainer.fit
+#     ckpt_path, wandb_run_id = None, None
+#     if cfg.auto_resume.enabled and cfg.resume_from_checkpoint is None:
+#         auto_resumer = AutoResumer(
+#             checkpoint_dir=os.path.join(cfg.checkpoint.dir, "linear"),
+#             max_hours=cfg.auto_resume.max_hours,
+#         )
+#         resume_from_checkpoint, wandb_run_id = auto_resumer.find_checkpoint(cfg)
+#         if resume_from_checkpoint is not None:
+#             print(
+#                 "Resuming from previous checkpoint that matches specifications:",
+#                 f"'{resume_from_checkpoint}'",
+#             )
+#             ckpt_path = resume_from_checkpoint
+#     elif cfg.resume_from_checkpoint is not None:
+#         ckpt_path = cfg.resume_from_checkpoint
+#         del cfg.resume_from_checkpoint
+
+#     callbacks = []
+
+#     if cfg.checkpoint.enabled:
+#         # save checkpoint on last epoch only
+#         ckpt = Checkpointer(
+#             cfg,
+#             logdir=os.path.join(cfg.checkpoint.dir, "linear"),
+#             frequency=cfg.checkpoint.frequency,
+#             keep_prev=cfg.checkpoint.keep_prev,
+#         )
+#         callbacks.append(ckpt)
+
+#     # wandb logging
+#     if cfg.wandb.enabled:
+#         wandb_logger = WandbLogger(
+#             name=cfg.name,
+#             project=cfg.wandb.project,
+#             entity=cfg.wandb.entity,
+#             offline=cfg.wandb.offline,
+#             resume="allow" if wandb_run_id else None,
+#             id=wandb_run_id,
+#         )
+#         wandb_logger.watch(model, log="gradients", log_freq=100)
+#         wandb_logger.log_hyperparams(OmegaConf.to_container(cfg))
+
+#     logs_root_dir = os.path.join(os.getcwd(), "benchmark_logs")
+
+#     tb_logge = TensorBoardLogger(
+#         name=cfg.name,
+#         save_dir=os.path.join(logs_root_dir, cfg.name),
+#         # project=cfg.wandb.project,
+#         sub_dir="linear",
+#         version=wandb_run_id,
+#     )
+#     tb_logge.log_hyperparams(OmegaConf.to_container(cfg))
+
+#         # lr logging
+#     lr_monitor = LearningRateMonitor(logging_interval="step")
+#     callbacks.append(lr_monitor)
+
+#     trainer_kwargs = OmegaConf.to_container(cfg)
+#     # we only want to pass in valid Trainer args, the rest may be user specific
+#     valid_kwargs = inspect.signature(Trainer.__init__).parameters
+#     trainer_kwargs = {name: trainer_kwargs[name] for name in valid_kwargs if name in trainer_kwargs}
+#     trainer_kwargs.update(
+#         {
+#             "logger": wandb_logger if cfg.wandb.enabled else tb_logge,
+#             "callbacks": callbacks,
+#             "enable_checkpointing": False,
+#             "strategy": DDPStrategy(find_unused_parameters=False)
+#             if cfg.strategy == "ddp"
+#             else cfg.strategy,
+#         }
+#     )
+#     trainer = Trainer(**trainer_kwargs)
+
+#     # fix for incompatibility with nvidia-dali and pytorch lightning
+#     # with dali 1.15 (this will be fixed on 1.16)
+#     # https://github.com/Lightning-AI/lightning/issues/12956
+#     try:
+#         from pytorch_lightning.loops import FitLoop
+
+#         class WorkaroundFitLoop(FitLoop):
+#             @property
+#             def prefetch_batches(self) -> int:
+#                 return 1
+
+#         trainer.fit_loop = WorkaroundFitLoop(
+#             trainer.fit_loop.min_epochs, trainer.fit_loop.max_epochs
+#         )
+#     except:
+#         pass
+
+#     if cfg.data.format == "dali":
+#         trainer.fit(model, ckpt_path=ckpt_path, datamodule=dali_datamodule)
+#     else:
+#         trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
+
+#     #trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
+
+
+# if __name__ == "__main__":
+#     main()
 # Copyright 2023 solo-learn development team.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -68,38 +304,16 @@ def main(cfg: DictConfig):
             backbone.maxpool = nn.Identity()
     
     # check if mixup or cutmix is enabled
-    mixup_func = None
-    mixup_active = cfg.mixup > 0 or cfg.cutmix > 0
-    if mixup_active:
-        logging.info("Mixup activated")
-        mixup_func = Mixup(
-            mixup_alpha=cfg.mixup,
-            cutmix_alpha=cfg.cutmix,
-            cutmix_minmax=None,
-            prob=1.0,
-            switch_prob=0.5,
-            mode="batch",
-            label_smoothing=cfg.label_smoothing,
-            num_classes=cfg.data.num_classes,
-        )
-        # smoothing is handled with mixup label transform
-        loss_func = SoftTargetCrossEntropy()
-    elif cfg.label_smoothing > 0:
-        loss_func = LabelSmoothingCrossEntropy(smoothing=cfg.label_smoothing)
-    else:
-        loss_func = torch.nn.CrossEntropyLoss()
+    loss_func = torch.nn.CrossEntropyLoss()
 
-    model = LinearModel(backbone, loss_func=loss_func, mixup_func=mixup_func, cfg=cfg)
+    model = LinearModel(backbone, loss_func=loss_func, mixup_func=None, cfg=cfg)
     make_contiguous(model)
     # can provide up to ~20% speed up
     if not cfg.performance.disable_channel_last:
         model = model.to(memory_format=torch.channels_last)
 
     
-    if cfg.data.format == "dali":
-        val_data_format = "image_folder"
-    else:
-        val_data_format = cfg.data.format
+    val_data_format = cfg.data.format
 
     train_loader, val_loader = prepare_data(
         cfg.data.dataset,
@@ -108,29 +322,9 @@ def main(cfg: DictConfig):
         data_format=val_data_format,
         batch_size=cfg.optimizer.batch_size,
         num_workers=cfg.data.num_workers,
-        auto_augment=cfg.auto_augment,
+        auto_augment=False,
         data_fraction=cfg.data.fraction
     )
-
-    if cfg.data.format == "dali":
-        assert (
-            _dali_avaliable
-        ), "Dali is not currently avaiable, please install it first with pip3 install .[dali]."
-
-        assert not cfg.auto_augment, "Auto augmentation is not supported with Dali."
-
-        dali_datamodule = ClassificationDALIDataModule(
-            dataset=cfg.data.dataset,
-            train_data_path=cfg.data.train_path,
-            val_data_path=cfg.data.val_path,
-            num_workers=cfg.data.num_workers,
-            batch_size=cfg.optimizer.batch_size,
-            data_fraction=cfg.data.fraction,
-            dali_device=cfg.dali.device,
-        )
-
-        # use normal torchvision dataloader for validation to save memory
-        dali_datamodule.val_dataloader = lambda: val_loader
 
     # 1.7 will deprecate resume_from_checkpoint, but for the moment
     # the argument is the same, but we need to pass it as ckpt_path to trainer.fit
@@ -224,12 +418,8 @@ def main(cfg: DictConfig):
     except:
         pass
 
-    if cfg.data.format == "dali":
-        trainer.fit(model, ckpt_path=ckpt_path, datamodule=dali_datamodule)
-    else:
-        trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
 
-    #trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
+    trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
 
 
 if __name__ == "__main__":
